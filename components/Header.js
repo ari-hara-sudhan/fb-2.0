4c2dca3dcf2d0cf4e9354c87ec8300d195ce2f4b
=ari <=ahshari2003@gmail.com>
2021-07-14 09:58:53 +0530

    header is done

--- components/Header.js
+++ components/Header.js
@@ -16,24 +16,31 @@ import {
 import HeaderIcon from "./HeaderIcon"
 function Header() {
     return (
-        <div>
+        <div className='sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md'>
            <div className='flex items-center'>
                <Image src="https://links.papareact.com/5me" width={40} height={40} layout="fixed"
                />
                <div className='flex ml-2 rounded-full bg-gray-100 p-2'>
                     <SearchIcon className='h-6 text-gray-600'/>
-                   <input className='flex ml-2 items-center bg-transparent outline-none placeholder-gray-500' type="text" placeholder="Search facebook"/>
+                   <input className=' hidden md:inline flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink' type="text" placeholder="Search facebook"/>
                </div>
            </div>
             <div className='flex justify-center flex-grow'>
                 <div className='flex space-x-6 md:space-x-2' >
-                    <HeaderIcon Icon={HomeIcon}/>
+                    <HeaderIcon Icon={HomeIcon} active />
                     <HeaderIcon Icon={FlagIcon}/>
                     <HeaderIcon Icon={PlayIcon}/>
                     <HeaderIcon Icon={ShoppingCartIcon}/>
                     <HeaderIcon Icon={UserGroupIcon}/>
 
                 </div>
+            </div>
+            <div className='flex items-center sm:space-x-2 justify-end'>
+                <p className="whitespace-nowrap font-semibold pr-3">ahs dev</p>
+                <ViewGridIcon className="icon"/>
+                <ChatIcon className="icon"/>
+                <BellIcon className="icon"/>
+                <ChevronDownIcon className="icon"/>
             </div>
         </div>
     )
